b3512f184a0169f3c23abf6418770c68854a8805
Lucas <morais.lucas.h@gmail.com>
2016-05-09 14:48:29 -0300

    Start sending build files to Lukensville/aux_integration_test

--- integration/send_zedboard.sh
+++ integration/send_zedboard.sh
@@ -30,9 +30,9 @@
 
 if [ $NOT_UNICAMP ]
 then
-	tar c libtioga.so mtsp_bridge.so | ssh -p 6868 lucas.morais@cajarana.lsc.ic.unicamp.br -t "ssh linaro@10.68.30.43 'tar x -C Lukensville/integration_test'"
+	tar c libtioga.so mtsp_bridge.so | ssh -p 6868 lucas.morais@cajarana.lsc.ic.unicamp.br -t "ssh linaro@10.68.30.43 'tar x -C Lukensville/aux_integration_test'"
 else
-	scp mtsp_bridge.so libtioga.so queue_test.cpp queue_test.h iotypes.h linaro@10.68.30.43:Lukensville/integration_test
+	scp mtsp_bridge.so libtioga.so queue_test.cpp queue_test.h iotypes.h linaro@10.68.30.43:Lukensville/aux_integration_test
 fi
 
 rm queue_test.cpp
