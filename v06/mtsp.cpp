c4198b94eb10dafd62c39a6de7454bb98ad3cfe0
Lucas <morais.lucas.h@gmail.com>
2017-06-13 16:06:27 -0300

    Avoid copies to mtsp_task_metadata variables inside __mtsp_RuntimeThreadCode; consider increment to tasksExecutedByRT for retirement profiling

--- v06/mtsp.cpp
+++ v06/mtsp.cpp
@@ -35,43 +35,6 @@ char ** __mtsp_taskMetadataBuffer;
   kmp_uint32 lastPrintedTaskId = 0;
 #endif
 
-
-
-//===-------- VTune/libittnotify related stuff ----------===//
-//__itt_domain* 		volatile __itt_mtsp_domain = nullptr;
-//__itt_string_handle* volatile __itt_CT_Fork_Call = nullptr;
-//__itt_string_handle* volatile __itt_Run_Queue_Dequeue = nullptr;
-//__itt_string_handle* volatile __itt_Run_Queue_Enqueue = nullptr;
-//__itt_string_handle* volatile __itt_Submission_Queue_Dequeue = nullptr;
-//__itt_string_handle* volatile __itt_Submission_Queue_Enqueue = nullptr;
-//__itt_string_handle* volatile __itt_Submission_Queue_Copy = nullptr;
-//__itt_string_handle* volatile __itt_Submission_Queue_Add = nullptr;
-//__itt_string_handle* volatile __itt_Retirement_Queue_Dequeue = nullptr;
-//__itt_string_handle* volatile __itt_Retirement_Queue_Enqueue = nullptr;
-//__itt_string_handle* volatile __itt_TaskGraph_Add = nullptr;
-//__itt_string_handle* volatile __itt_TaskGraph_Del = nullptr;
-//__itt_string_handle* volatile __itt_Checking_Dependences = nullptr;
-//__itt_string_handle* volatile __itt_Releasing_Dependences = nullptr;
-//__itt_string_handle* volatile __itt_Releasing_Dep_Reader = nullptr;
-//__itt_string_handle* volatile __itt_CT_Barrier_Wait = nullptr;
-//__itt_string_handle* volatile __itt_CT_Task_Alloc = nullptr;
-//__itt_string_handle* volatile __itt_CT_Task_With_Deps = nullptr;
-//__itt_string_handle* volatile __itt_WT_Barrier = nullptr;
-//__itt_string_handle* volatile __itt_WT_Serving_Steal = nullptr;
-//__itt_string_handle* volatile __itt_WT_Wait_For_Work = nullptr;
-//__itt_string_handle* volatile __itt_Task_In_Execution = nullptr;
-//__itt_string_handle* volatile __itt_Task_Stealing = nullptr;
-//__itt_string_handle* volatile __itt_SPSC_Enq = nullptr;
-//__itt_string_handle* volatile __itt_SPSC_Deq = nullptr;
-//__itt_string_handle* volatile __itt_SPSC_Enq_Blocking = nullptr;
-//__itt_string_handle* volatile __itt_SPSC_Deq_Blocking = nullptr;
-
-
-//__itt_string_handle* volatile __itt_RT_Main_Loop = nullptr;
-//__itt_string_handle* volatile __itt_RT_Check_Del = nullptr;
-//__itt_string_handle* volatile __itt_RT_Check_Add = nullptr;
-//__itt_string_handle* volatile __itt_RT_Check_Oth = nullptr;
-
 extern void steal_from_single_run_queue(bool just_a_bit);
 extern void steal_from_multiple_run_queue(bool just_a_bit);
 
@@ -111,38 +74,7 @@ void debug() {
 }
 
 void __mtsp_initialize() {
-  //	__itt_mtsp_domain = __itt_domain_create("MTSP");
-  //	__itt_CT_Fork_Call = __itt_string_handle_create("CT_Fork_Call");
-  //	__itt_Run_Queue_Dequeue = __itt_string_handle_create("Run_Queue_Dequeue");
-  //	__itt_Run_Queue_Enqueue = __itt_string_handle_create("Run_Queue_Enqueue");
-  //	__itt_Submission_Queue_Dequeue = __itt_string_handle_create("Submission_Queue_Dequeue");
-  //	__itt_Submission_Queue_Enqueue = __itt_string_handle_create("Submission_Queue_Enqueue");
-  //	__itt_Submission_Queue_Copy = __itt_string_handle_create("Submission_Queue_Copy");
-  //	__itt_Submission_Queue_Add = __itt_string_handle_create("Submission_Queue_Add");
-  //	__itt_Retirement_Queue_Dequeue = __itt_string_handle_create("Retirement_Queue_Dequeue");
-  //	__itt_Retirement_Queue_Enqueue = __itt_string_handle_create("Retirement_Queue_Enqueue");
-  //	__itt_TaskGraph_Add = __itt_string_handle_create("TaskGraph_Add");
-  //	__itt_TaskGraph_Del = __itt_string_handle_create("TaskGraph_Del");
-  //	__itt_Checking_Dependences = __itt_string_handle_create("Checking_Dependences");
-  //	__itt_Releasing_Dependences = __itt_string_handle_create("Releasing_Dependences");
-  //	__itt_Releasing_Dep_Reader = __itt_string_handle_create("Releasing_Dep_Reader");
-  //	__itt_CT_Barrier_Wait = __itt_string_handle_create("CT_Barrier_Wait");
-  //	__itt_CT_Task_Alloc = __itt_string_handle_create("CT_Task_Alloc");
-  //	__itt_CT_Task_With_Deps = __itt_string_handle_create("CT_Task_With_Deps");
-  //	__itt_WT_Barrier = __itt_string_handle_create("WT_Barrier");
-  //	__itt_WT_Serving_Steal = __itt_string_handle_create("WT_Serving_Steal");
-  //	__itt_WT_Wait_For_Work = __itt_string_handle_create("WT_Wait_For_Work");
-  //	__itt_Task_In_Execution = __itt_string_handle_create("Task_In_Execution");
-  //	__itt_Task_Stealing = __itt_string_handle_create("Task_Stealing");
-  //	__itt_SPSC_Enq = __itt_string_handle_create("SPSC_Enq");
-  //	__itt_SPSC_Deq = __itt_string_handle_create("SPSC_Deq");
-  //	__itt_SPSC_Enq_Blocking = __itt_string_handle_create("SPSC_Enq_Blocking");
-  //	__itt_SPSC_Deq_Blocking = __itt_string_handle_create("SPSC_Deq_Blocking");
 
-  //	__itt_RT_Main_Loop = __itt_string_handle_create("RT_Main_Loop");
-  //	__itt_RT_Check_Del = __itt_string_handle_create("RT_Check_Del");
-  //	__itt_RT_Check_Add = __itt_string_handle_create("RT_Check_Add");
-  //	__itt_RT_Check_Oth = __itt_string_handle_create("RT_Check_Oth");
 
   __mtsp_taskMetadataStatus = (bool volatile *) malloc(MAX_TASKMETADATA_SLOTS * sizeof(bool volatile));
   __mtsp_taskMetadataBuffer = (char **) malloc(MAX_TASKMETADATA_SLOTS * sizeof(char *));
@@ -168,7 +100,6 @@ void __mtsp_initialize() {
       }
 
 void __mtsp_addNewTask(kmp_task* newTask, kmp_uint32 ndeps, kmp_depend_info* depList) {
-  //	__itt_task_begin(__itt_mtsp_domain, __itt_null, __itt_null, __itt_Submission_Queue_Enqueue);
 
 #ifdef MTSP_WORKSTEALING_CT
   /// The CT is trying to submit work but the queue is full. The CT will then
@@ -203,11 +134,9 @@ void __mtsp_RuntimeWorkSteal() {
   /// Counter for the total cycles spent per task
   unsigned long long start=0, end=0;
 
-  //	__itt_task_begin(__itt_mtsp_domain, __itt_null, __itt_null, __itt_Task_Stealing);
 
   while (RunQueue.cur_load() > RunQueue.cont_load()) {
     if (RunQueue.try_deq(&taskToExecute)) {
-      //			 __itt_task_begin(__itt_mtsp_domain, __itt_null, __itt_null, __itt_Task_In_Execution);
 
 #ifdef MEASURE_TASK_SIZE
       start = beg_read_mtsp();
@@ -220,29 +149,24 @@ void __mtsp_RuntimeWorkSteal() {
       end = end_read_mtsp();
 #endif
 
-      //			__itt_task_end(__itt_mtsp_domain);
-
 #ifdef MEASURE_TASK_SIZE
       taskToExecute->metadata->cycles_execution = (end - start);
 #endif
 
-      tasksExecutedByRT++;
-
-      /// Inform that this task has finished execution
-      //			__itt_task_begin(__itt_mtsp_domain, __itt_null, __itt_null, __itt_Retirement_Queue_Enqueue);
 #ifdef MEASURE_RETIREMENT
       start = beg_read_mtsp();
 #endif
+      tasksExecutedByRT++;
+
+      /// Inform that this task has finished execution
       RetirementQueue.enq(taskToExecute);
 #ifdef MEASURE_RETIREMENT
       end = end_read_mtsp();
       taskToExecute->metadata->cycles_retirement = (end - start);
 #endif
-      //			__itt_task_end(__itt_mtsp_domain);
     }
   }
 
-  //	__itt_task_end(__itt_mtsp_domain);
 }
 
 void* __mtsp_RuntimeThreadCode(void* params) {
@@ -250,7 +174,10 @@ void* __mtsp_RuntimeThreadCode(void* params) {
   kmp_task* task = nullptr;
   kmp_uint64 iterations = 0;
   kmp_uint64 BatchSize = (64 - 1);	// should be 2^N - 1
-  mtsp_task_metadata metadata_of_retiring_task;
+
+#ifdef MEASURE_RETIREMENT
+  kmp_uint32 retiring_task_id;
+#endif
 
   while (true) {
     /// Check if the execution of a task has been completed
@@ -258,26 +185,24 @@ void* __mtsp_RuntimeThreadCode(void* params) {
     kmp_uint64 start = beg_read_mtsp();
 #endif
     if (RetirementQueue.try_deq(&task)) {
-      //TODO: Make this faster by only copying the globalTaskId and
-      //      storing the end values of other fields directly to the
-      //      taskMetadata array
-      metadata_of_retiring_task = *(task->metadata);
+#ifdef MEASURE_RETIREMENT
+      retiring_task_id = task->metadata->globalTaskId;
+      taskMetadata[retiring_task_id].cycles_execution = task->metadata->cycles_execution;
+      taskMetadata[retiring_task_id].cycles_addition = task->metadata->cycles_addition;
+      taskMetadata[retiring_task_id].cycles_allocation = task->metadata->cycles_allocation;
+#endif
       removeFromTaskGraph(task);
 
 #ifdef MEASURE_RETIREMENT
       kmp_uint64 end = end_read_mtsp();
-      metadata_of_retiring_task.cycles_retirement += (end - start);
+      taskMetadata[retiring_task_id].cycles_retirement += (end - start);
 #endif
 
 #ifdef MEASURE_TASK_SIZE
-      if (metadata_of_retiring_task.globalTaskId >= MAXIMUM_EXPECTED_TASKS) {
+      if (retiring_task_id >= MAXIMUM_EXPECTED_TASKS) {
         printf("Critical: Maximum_expected_tasks reached at %s:%d\n", __FILE__, __LINE__);
         exit(-1);
       }
-      taskMetadata[metadata_of_retiring_task.globalTaskId].cycles_execution = metadata_of_retiring_task.cycles_execution;
-      taskMetadata[metadata_of_retiring_task.globalTaskId].cycles_retirement = metadata_of_retiring_task.cycles_retirement;
-      taskMetadata[metadata_of_retiring_task.globalTaskId].cycles_addition = metadata_of_retiring_task.cycles_addition;
-      taskMetadata[metadata_of_retiring_task.globalTaskId].cycles_allocation = metadata_of_retiring_task.cycles_allocation;
 #endif
     }
 
